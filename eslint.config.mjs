54ddefce234ad3d38d4889a07a277d516b0c6b13
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-09-28 21:14:59 +0530

    Deploy Tweaks 2.1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,7 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
-      "app/src/generated/**/*.{js,ts}"
+      "src/app/generated/**/*.{js,ts}"
     ],
     
   },
